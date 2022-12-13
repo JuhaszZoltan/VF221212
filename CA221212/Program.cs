cd342217901b1616a2b67d6065690a512de1109a
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-12-13 08:39:17 +0100

    add frm prj

--- CA221212/Program.cs
+++ CA221212/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CA221212;
 
+Random rnd = new();
 List<Game> games = new();
 using StreamReader sr = new("..\\..\\..\\RES\\bestgames.csv");
 while (!sr.EndOfStream) games.Add(new Game(sr.ReadLine()!));
@@ -15,20 +16,25 @@ var grp = games.GroupBy(g => g.ReleaseYear)
 Console.WriteLine($"f2: ezekben az években került több, mint 10 cím a listára:");
 foreach (var kvp in grp) Console.WriteLine($"\t{kvp.Key}: {kvp.Value}db");
 
-Console.WriteLine("vagy:");
-Dictionary<int, int> dic = new();
-foreach (var g in games)
-    if (!dic.ContainsKey(g.ReleaseYear)) dic.Add(g.ReleaseYear, 1);
-    else dic[g.ReleaseYear]++;
-List<(int ev, int db)> lisata = new();
-foreach (var kvp in dic)
-    if (kvp.Value > 10) lisata.Add((kvp.Key, kvp.Value));
-for (int i = 0; i < lisata.Count - 1; i++)
-    for (int j = i + 1; j < lisata.Count; j++)
-        if (lisata[i].db < lisata[j].db)
-            (lisata[i], lisata[j]) = (lisata[j], lisata[i]);
-foreach (var e in lisata) Console.WriteLine($"\t{e.ev}: {e.db}db");
+//Console.WriteLine("vagy:");
+//Dictionary<int, int> dic = new();
+//foreach (var g in games)
+//    if (!dic.ContainsKey(g.ReleaseYear)) dic.Add(g.ReleaseYear, 1);
+//    else dic[g.ReleaseYear]++;
+//List<(int ev, int db)> lisata = new();
+//foreach (var kvp in dic)
+//    if (kvp.Value > 10) lisata.Add((kvp.Key, kvp.Value));
+//for (int i = 0; i < lisata.Count - 1; i++)
+//    for (int j = i + 1; j < lisata.Count; j++)
+//        if (lisata[i].db < lisata[j].db)
+//            (lisata[i], lisata[j]) = (lisata[j], lisata[i]);
+//foreach (var e in lisata) Console.WriteLine($"\t{e.ev}: {e.db}db");
+
+Game[] shooters = games.Where(g => g.Genre == "First-person shooter").ToArray();
+string rndShooter = shooters[rnd.Next(shooters.Length)].Title;
+
+Console.WriteLine(
+    $"f3: összesen {shooters.Length} FPS került fel a listára,\n\t" +
+    $"például: {rndShooter}");
 
 Console.ReadKey(true);
-
-
